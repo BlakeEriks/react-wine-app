4bafb61768f7e236c235b73dc3dff70a54dffc22
Blake Eriks <eblake98@vt.edu>
2021-11-09 14:40:02 -1000

    Add update + delete calls

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,13 +9,15 @@ const Main = () => {
     const [mode, setMode] = useState('create')
     const [selectedWine, setSelectedWine] = useState(null)
 
-    const URL = 'https://express-react-wine.herokuapp.com/wines'
+    const URL = 'https://express-react-wine.herokuapp.com/wines/'
 
     const getWines = async () => {
         const response = await fetch(URL)
         const data = await response.json()
         setWines(data)
     }
+
+    useEffect( () => getWines(), [])
 
     const createWine = async (wine) => {
         await fetch(URL, {
@@ -25,17 +27,32 @@ const Main = () => {
             },
             body: JSON.stringify(wine)
         })
-
         getWines()
     }
 
-    useEffect( () => getWines(), [])
+    const updateWine = async (wine) => {
+        await fetch(URL + wine._id, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(wine)
+        })
+        getWines()
+    }
+
+    const deleteWine = async (wine) => {
+        await fetch(URL + wine._id, {
+            method: 'delete'
+        })
+        getWines()
+    }
 
     return (
         <main>
             <Index wines={wines} selectedWine={selectedWine} setSelectedWine={setSelectedWine} setMode={setMode}/>
             {mode === 'show' &&
-            <Show wine={wines[selectedWine]} />}
+            <Show wine={wines[selectedWine]} updateWine={updateWine} deleteWine={deleteWine}/>}
             {mode === 'create' &&
             <Create  createWine={createWine}/>
             }
